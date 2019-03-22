9a352a4f15b7c417f9c698a3a7473623e0dea452
kris <kriscen@163.com>
2019-03-22 22:45:39 +0800

    idea创建maven父子工程，基于rest创建服务提供者和消费者。

--- microservicecloud-consumer-dept-80/src/main/java/com/kris/springcloud/DeptConsumer80App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/kris/springcloud/DeptConsumer80App.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeptConsumer80App {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer80App.class,args);
+
     }
 }
