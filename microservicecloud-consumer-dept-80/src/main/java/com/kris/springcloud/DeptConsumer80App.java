3e2d66f020b7890d3c2cba13610a12b59b2085b4
kris <kriscen@163.com>
2019-03-26 16:42:00 +0800

    Ribbn负载均衡策略替换

--- microservicecloud-consumer-dept-80/src/main/java/com/kris/springcloud/DeptConsumer80App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/kris/springcloud/DeptConsumer80App.java
@@ -1,11 +1,15 @@
 package com.kris.springcloud;
 
+import com.kris.myrule.MyselfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//特殊化定义rule，不能放在主启动类包及子包内
+//@RibbonClient(name = "MICROSERVICECLOUD-DEPT",configuration = MyselfRule.class)
 public class DeptConsumer80App {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer80App.class,args);
--- microservicecloud-consumer-dept-80/src/main/java/com/kris/springcloud/cfgbean/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/kris/springcloud/cfgbean/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.kris.springcloud.cfgbean;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +11,14 @@ import org.springframework.web.client.RestTemplate;
 public class ConfigBean {
 
     @Bean
-    @LoadBalanced//客户端负载均衡工具
+    //客户端负载均衡工具
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+    @Bean
+    public IRule myRule(){
+        //使用指定算法替换默认轮询
+        return new RandomRule();
     }
 }
