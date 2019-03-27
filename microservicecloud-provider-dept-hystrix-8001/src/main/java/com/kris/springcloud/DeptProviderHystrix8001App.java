fb0924e9f2b2c1849fbc689d2aed2570acd2f9d5
kris <kriscen@163.com>
2019-03-27 15:22:36 +0800

    单个方法Hystrix服务熔断

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/kris/springcloud/DeptProviderHystrix8001App.java
@@ -0,0 +1,19 @@
+package com.kris.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient//自动注册eureka服务
+@EnableDiscoveryClient//服务发现
+@EnableCircuitBreaker//熔断器
+public class DeptProviderHystrix8001App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProviderHystrix8001App.class,args);
+    }
+}
---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/kris/springcloud/controller/DeptController.java
@@ -0,0 +1,33 @@
+package com.kris.springcloud.controller;
+
+import com.kris.springcloud.entities.Dept;
+import com.kris.springcloud.service.DeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DeptController {
+    @Autowired
+    private DeptService service = null;
+
+    @RequestMapping(value = "/dept/{id}", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
+    public Dept get(@PathVariable("id") Long id) {
+        Dept dept = this.service.get(id);
+        if (null == dept) {
+            throw new RuntimeException("该ID：" + id + "没有没有对应的信息");
+        }
+        return dept;
+    }
+
+    public Dept processHystrix_Get(@PathVariable("id") Long id) {
+        return new Dept().setDeptNo(id)
+                .setDName("该ID：" + id + "没有没有对应的信息,null--@HystrixCommand")
+                .setDbSource("no this database in MySQL");
+    }
+}
